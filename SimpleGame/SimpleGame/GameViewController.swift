569fc221641ca045d635541cf4ceb141fe6746cb
Naveen Naidu <mnaveennaidu9927@gmail.com>
2017-07-17 21:31:33 +0530

    Learning SpriteKit

---
+++ SimpleGame/SimpleGame/GameViewController.swift
@@ -0,0 +1,29 @@
+//
+//  GameViewController.swift
+//  SimpleGame
+//
+//  Created by Naveen Naidu  on 17/07/17.
+//  Copyright © 2017 Naveen Naidu . All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class GameViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let scene = GameScene(size: view.bounds.size)
+        let skView = view as! SKView
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        skView.ignoresSiblingOrder = true
+        scene.scaleMode = .resizeFill
+        skView.presentScene(scene)
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+}
